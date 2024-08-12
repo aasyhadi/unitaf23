30b3cb6465b098bfd67aa7e98e0291ea4cb52f0c
aasyhadi <adiproojambi@gmail.com>
2024-08-12 07:22:40 +0700

    klik baris baru2

--- resources/views/backend/penjualan/update.blade.php
+++ resources/views/backend/penjualan/update.blade.php
@@ -204,8 +204,7 @@
             var jumlah = $(wrapper).find('input[name="jumlah[]"]:last').val();
             $.ajax({
                 type: 'GET',
-                url: 'https://unitaf.my.id/backend/penjualan/check-stok',
-                dataType: "jsonp",
+                url: 'https://unitaf.my.id/penjualan/check-stok',
                 data: {
                     id_bahan_baku: id_bahan_baku,
                     jumlah: jumlah
